3ff0d4740f6f3b6924c93a08c864a4840fc8d1e1
elizabethseto <elizabethwseto@gmail.com>
2023-04-09 11:30:35 -0700

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 import Image from "next/legacy/image";
-import me from "../public/me.jpeg";
+import me from "public/me.jpeg";
 
 export default function Home() {
 
@@ -37,18 +37,18 @@ export default function Home() {
         <section>
           <div>
             <h3 className="text-center text-3xl py-1">About Me</h3>
-            <p className="text-lg py-2 leading-8 text-gray">
+            <p className="text-md py-2 leading-8 text-gray">
               I'm recent graduate from the University of Massachusetts Amherst with a computer science degree. Currently, I'm a <span className="text-pink-500">software engineer</span> at <span className="text-pink-500">Liberty Mutual Insurance</span>, building cloud products and infrastructure for application teams.
             </p>
           </div>
           <div className="lg:flex gap-5">
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <h3 className="text-2xl font-medium pt-8 pb-2">Education</h3>
-              <p className="text-lg py-2">
+              <h3 className="text-xl font-medium pt-8 pb-2">Education</h3>
+              <p className="text-md py-2">
                 University of Massachusetts Amherst, 2019 - 2022
               </p>
-              <p className="text-lg font-medium">Relevant Coursework:</p>
-              <ul className="text-md">
+              <p className="text-sm font-medium">Relevant Coursework:</p>
+              <ul className="text-sm">
                 <li>INFO 203: A Networked World</li>
                 <li>CS 311: Algorithms</li>
                 <li>CS 383: Artificial Intelligence</li>
@@ -62,7 +62,7 @@ export default function Home() {
                 <li>CS 563: Internet Law and Policy</li>
                 <li>MATH 235: Linear Algebra</li>
                 <li>MATH 233: Multivariate Calculus</li>
-                <li>CS 377: OPerating Systems</li>
+                <li>CS 377: Operating Systems</li>
                 <li>PHYSICS 151: Physics I</li>
                 <li>CS 345: Practice and Applications of Data Management</li>
                 <li>CS 121: Problem Solving with Computers</li>
@@ -75,10 +75,10 @@ export default function Home() {
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
               <h3 className="text-2xl font-medium pt-8 pb-2">Skills</h3>
-              <p className="text-md py-2 leading-8 text-gray md:text-xl">
+              <p className="text-sm py-2 leading-8 text-gray md:text-xl">
                 Here are some other <span className="text-pink-500">technologies</span> I work with:
               </p>
-              <ul className="text-md">
+              <ul className="text-sm">
                 <li>Amazon Web Services</li>
                 <li>Microsoft Azure</li>
                 <li>Angular</li>
@@ -93,27 +93,27 @@ export default function Home() {
             </div>
             <div className="text-left shadow-lg p-10 rounded-xl my-10">
               <h3 className="text-2xl text-center font-medium pt-8 pb-2">Experience</h3>
-              <p className="text-lg font-medium">Associate Software Engineer, Liberty Mutual Insurance</p>
+              <p className="text-md font-medium">Associate Software Engineer, Liberty Mutual Insurance</p>
               <p>January 2023 - PRESENT</p>
               <br></br>
-              <p><span className="text-sm font-medium">Skill:</span> Amazon Web Services (AWS) · Angular · JavaScript · Jest · Microsoft Azure · Node.js · Python (Programming Language) · TypeScript</p>
+              <p className="text-sm"><span className="text-sm font-medium">Skill:</span> Amazon Web Services (AWS) · Angular · JavaScript · Jest · Microsoft Azure · Node.js · Python (Programming Language) · TypeScript</p>
               <br></br>
-              <p className="text-lg font-medium">Technology Associate, Liberty Mutual Insurance</p>
+              <p className="text-md font-medium">Technology Associate, Liberty Mutual Insurance</p>
               <p>September 2022 - January 2023</p>
               <br></br>
-              <p><span className="text-sm font-medium">Skill:</span> Amazon Web Services (AWS) · Angular · JavaScript · Jest · Microsoft Azure · Node.js · Python (Programming Language) · TypeScript</p>
+              <p className="text-sm"><span className="text-sm font-medium">Skill:</span> Amazon Web Services (AWS) · Angular · JavaScript · Jest · Microsoft Azure · Node.js · Python (Programming Language) · TypeScript</p>
               <br></br>
-              <p className="text-lg font-medium">Software Engineer Intern, Liberty Mutual Insurance</p>
+              <p className="text-md font-medium">Software Engineer Intern, Liberty Mutual Insurance</p>
               <p>June 2022 - September 2022</p>
-              <p>Full-stack development on Architecture, Cloud and Engineering Enablement Team; build cloud infrastructure and products to support application teams.</p>
+              <p className="text-sm">Full-stack development on Architecture, Cloud and Engineering Enablement Team; build cloud infrastructure and products to support application teams.</p>
               <br></br>
-              <p><span className="text-sm font-medium">Skill:</span> Amazon Web Services (AWS) · Angular · JavaScript · Jest · Microsoft Azure · Node.js · Python (Programming Language) · TypeScript</p>
+              <p className="text-sm"><span className="text-sm font-medium">Skill:</span> Amazon Web Services (AWS) · Angular · JavaScript · Jest · Microsoft Azure · Node.js · Python (Programming Language) · TypeScript</p>
               <br></br>
-              <p className="text-lg font-medium">Software Development Co-op, Solaria Labs</p>
-              <p>January 2022 - June 2022</p>
+              <p className="text-md font-medium">Software Development Co-op, Solaria Labs</p>
+              <p className="text-sm">January 2022 - June 2022</p>
               <p>Full-stack development in Dwellbeing app, an Android and iOS application for personalized home maintenance tasks; implemented/debugged multiple frontend and backend features.</p>
               <br></br>
-              <p><span className="text-sm font-medium">Skill:</span> Amazon Web Services (AWS) · Dart · Express.js · Flutter · JavaScript · MySQL · Node.js · React.js</p>
+              <p className="text-sm"><span className="text-sm font-medium">Skill:</span> Amazon Web Services (AWS) · Dart · Express.js · Flutter · JavaScript · MySQL · Node.js · React.js</p>
             </div>
           </div>
         </section>
